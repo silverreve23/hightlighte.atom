1d740ea4ed3adfdf107e87b70cff234a995602b6
Sviatoslav Sabadosh <silverreve23@gmail.com>
2017-12-03 14:51:42 +0200

    commit to github

---
+++ spec/highlighte-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HighlighteView from '../lib/highlighte-view';
+
+describe('HighlighteView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
